2ad0cd828d98c2c461c0fd9bf7739b6072e1607e
Cam T. Bui <ncbui@users.noreply.github.com>
2020-08-17 16:19:03 -0700

    add login route to handle user login and pass token to user if login successful.

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,33 @@
 "use strict";
 
+/**Handle calls to /auth routes */
+
 const Router = require("express").Router;
-const router = new Router();
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config");
+const { UnauthorizedError } = require("../expressError");
 
+const router = new Router();
 
 /** POST /login: {username, password} => {token} */
+router.post("/login", async (req, res, next) => {
 
+  try {
+    const { username, password } = req.body;
+    const loggedInUser = await User.authenticate(username, password);
+    console.log("loggedInUser: ", loggedInUser)
+
+    if (loggedInUser) {
+      const token = jwt.sign({ username }, SECRET_KEY);
+      return res.json({ token });
+    }
+    throw new UnauthorizedError("Invalid user/password");
+
+  } catch (err) {
+    return next(err);
+  }
+});
 
 /** POST /register: registers, logs in, and returns token.
  *
@@ -18,14 +37,14 @@ router.post("/register", async function (req, res, next) {
   console.log("in register route");
   try {
     const { username, password, first_name, last_name, phone } = req.body;
-    const newUser = await User.register({username, password, first_name, last_name, phone});
+    const newUser = await User.register({ username, password, first_name, last_name, phone });
     console.log("after registering and this is newUser:", newUser);
 
-    let payload = {username};
+    let payload = { username };
     let token = jwt.sign(payload, SECRET_KEY);
     console.log("this is payload", payload, "this is token:", token);
 
-    return res.json({token});
+    return res.json({ token });
   } catch (err) {
     return next(err);
   }
